d7ee2b15971b21728505731bef5915a0dd5b7124
Jonas Wiener <wiener.jonas1@gmail.com>
2026-06-11 19:53:27 +0200

    fix: struct name and typo

--- src/layer_rgb.c
+++ src/layer_rgb.c
@@ -4,25 +4,25 @@
 #include <zmk/rgb_underglow.h>
 #include <zmk/keymap.h>
 
-static int laye_color_listener_cb(const zmk_event_t *eh) {
+static int layer_color_listener_cb(const zmk_event_t *eh) {
     uint8_t active_layer = zmk_keymap_highest_layer_active();
 
     switch (active_layer) {
         case 1:
             /* Turn Green for Layer 1 */
-            zmk_rgb_underglow_set_hsb((struct zmk_color_hsb){119, 100, 100});
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){119, 100, 100});
             break;
         case 2:
             /* Turn Orange for Layer 2 */
-            zmk_rgb_underglow_set_hsb((struct zmk_color_hsb){31, 100, 100});
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){31, 100, 100});
             break;
         case 3:
             /* Turn Pink for Layer 3 */
-            zmk_rgb_underglow_set_hsb((struct zmk_color_hsb){344, 100, 100});
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){344, 100, 100});
             break;
         default:
             /* Turn Blue for Default Base Layer */
-            zmk_rgb_underglow_set_hsb((struct zmk_color_hsb){193, 100, 100});
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){193, 100, 100});
             break;
     }
     return ZMK_EV_EVENT_BUBBLE;
